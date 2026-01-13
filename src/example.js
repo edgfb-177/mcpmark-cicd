b4df4b9215077147897ed1d70d7aa6bed825f74a
edgfb-177 <reyrryi5@013414.xyz>
2026-01-14 03:18:49 +0800

    Add ESLint config, lint workflow, and example file with linting errors

---
+++ src/example.js
@@ -0,0 +1,9 @@
+// This file has intentional linting errors
+const unusedVar = "this is unused";
+
+function example() {
+  console.log("Hello world")
+  let anotherUnused = "also unused";
+}
+
+example();
